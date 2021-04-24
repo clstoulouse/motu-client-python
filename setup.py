5f6c02e97810ad4cc47de7af28f22de34639cd48
Hajduch Guillaume <ghajduch@cls.fr>
2021-04-24 08:46:00 +0200

    Library_path changes must be avoided as Python package already handles this + move POM version to setup.py as this is the single location were it is used and the package directory structure was not yet defined at import of pom_version

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
 import os
-import sys
 
 from setuptools import setup, find_packages
+from xml.etree import ElementTree
 
-# project libraries path
-LIBRARIES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src', 'python')
 
-# Manage imports of project libraries
-if not os.path.exists(LIBRARIES_PATH):
-    sys.stderr.write('\nERROR: can not find project libraries path: %s\n\n' % os.path.abspath(LIBRARIES_PATH))
-    sys.exit(1)
-sys.path.append(LIBRARIES_PATH)
+def getPOMVersion():
+    version = "Unknown"
+    try:
+        # For production tree, while run from cur folder
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        version = __getPOMVersion(os.path.join(dname, "..", "pom.xml"))
+    except Exception:
+        # For development tree
+        try:
+            version = __getPOMVersion(
+                os.path.join(dname, "..", "..", "pom.xml"))
+        except Exception:
+            version = __getPOMVersion(os.path.join(
+                dname, "..", "..", "..", "pom.xml"))
+    finally:
+        return version
 
-from motu_utils import pom_version
+
+def __getPOMVersion(POM_FILE):
+    namespaces = {'xmlns': 'http://maven.apache.org/POM/4.0.0'}
+    tree = ElementTree.parse(POM_FILE)
+    root = tree.getroot()
+    version = root.find("xmlns:version", namespaces=namespaces)
+    return str(version.text)
+
 
 setup(
     name='motuclient',
-    version=pom_version.getPOMVersion(),
+    version=getPOMVersion(),
     python_requires='>=2.7',
     description='Extract and download gridded data through a python command line from Motu web server. Used in CMEMS context http://marine.copernicus.eu/',
     long_description='Motu is a high efficient and robust Web Server which fills the gap between heterogeneous data providers to end users. Motu handles, extracts and transforms oceanographic huge volumes of data without performance collapse. This client enables to extract and download data through a python command line.',
@@ -36,9 +53,9 @@ setup(
     url='https://github.com/clstoulouse/motu-client-python',
     license='LGPL',
 
-    package_dir = {'': 'src/python', 'motu_utils': 'src/python/motu_utils'},
-    packages=['motu_utils'],
-    py_modules = ['motuclient', 'motu-client'],
+    package_dir={'': 'src/python'},
+    packages=find_packages('src/python'),
+    py_modules=['motuclient', 'motu-client'],
     include_package_data=True,
 
     download_url='https://github.com/clstoulouse/motu-client-python/releases/',
